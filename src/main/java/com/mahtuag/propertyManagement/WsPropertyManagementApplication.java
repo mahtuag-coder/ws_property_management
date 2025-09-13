a3a7b4306a83bdb11913f260da59aee7385d71f9
mahtuag-coder <gautham.malapati@gmail.com>
2025-09-13 11:35:29 -0500

    Initial commmit

---
+++ src/main/java/com/mahtuag/propertyManagement/WsPropertyManagementApplication.java
@@ -0,0 +1,13 @@
+package com.mahtuag.propertyManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsPropertyManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsPropertyManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mahtuag/propertyManagement/WsPropertyManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahtuag.propertyManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsPropertyManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
