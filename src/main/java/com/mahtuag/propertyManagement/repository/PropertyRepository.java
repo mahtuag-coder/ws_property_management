b8cc9271989e6e22f65125f06e65f36798048101
mahtuag-coder <gautham.malapati@gmail.com>
2025-09-13 15:11:55 -0500

    Implemented Pagination

--- src/main/java/com/mahtuag/propertyManagement/repository/PropertyRepository.java
+++ src/main/java/com/mahtuag/propertyManagement/repository/PropertyRepository.java
@@ -1,9 +1,15 @@
 package com.mahtuag.propertyManagement.repository;
 
 import com.mahtuag.propertyManagement.entity.Property;
+import com.mahtuag.propertyManagement.model.enums.PropertyStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PropertyRepository extends JpaRepository<Property, Long> {
+    Page<Property> getPropertiesByStatus(PropertyStatus status, Pageable pageable);
 }
--- src/main/java/com/mahtuag/propertyManagement/services/PropertyService.java
+++ src/main/java/com/mahtuag/propertyManagement/services/PropertyService.java
@@ -2,12 +2,17 @@ package com.mahtuag.propertyManagement.services;
 
 
 import com.mahtuag.propertyManagement.entity.Property;
+import com.mahtuag.propertyManagement.model.enums.PropertyStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface PropertyService {
 
-    List<Property> getAllProperties();
+    Page<Property> getAllProperties(Pageable pageable);
 
     Property getPropertyById(Long id);
+
+    Page<Property> getPropertiesByStatus(PropertyStatus status, Pageable pageable);
 }
